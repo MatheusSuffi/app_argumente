6d6c417a28549c31dd36cfd58d20e4bcb0c63f57
Matheus <matheus@matheus.matheus>
2020-02-14 09:26:44 -0300

    first commit + creating the login screen

---
+++ views/Login.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+import {
+        View,
+        Text, 
+        ImageBackground, 
+        StyleSheet,
+        TextInput,
+        Image,
+        Linking,
+        Button,
+        TouchableHighlight
+    } from 'react-native'
+
+export default class Login extends Component {
+    render() {
+        return(
+            <ImageBackground source={require('../assets/imgs/login.jpg')} style={styles.imageLogin}>
+                <View style={styles.content} >                       
+                    <Image source={require('../assets/imgs/logo_argumente.png')} style={styles.logo}/> 
+                    <TextInput placeholder="Usuario" style={styles.textFields} />          
+                    <TextInput placeholder="Senha" style={styles.textFields}/>
+                    <Text style={styles.link}
+                        onPress={() => Linking.openURL('http://google.com')}>
+                        Registre-se
+                    </Text>
+                    <TouchableHighlight style={styles.buttons}>
+                        <Button title="Login" />
+                    </TouchableHighlight>
+                </View>        
+            </ImageBackground>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    imageLogin:{
+        flex:1,
+        width:null,
+        height: null
+    },content:{
+        flex: 1, 
+        alignItems: 'center', 
+        marginTop: 10
+    },textFields:{
+        backgroundColor: '#AAA',
+        width:'70%',
+        height:40,
+        marginTop: 20
+    },
+    logo:{
+        marginTop:20,
+        marginBottom:30,
+        width:170,
+        height:150
+    },
+    link:{
+        color:'blue',
+        width:'70%'
+    },buttons:{
+        width:'70%',
+        height:40,
+        marginTop: 20
+    }
+})
